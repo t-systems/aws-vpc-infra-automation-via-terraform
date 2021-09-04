5ed92ab1b8d8176d9bc2f0d7b03626770893db01
vivek22117 <vivekmishra22117@gmail.com>
2021-09-04 20:22:15 +0530

    adding data file to read VPC TF state

--- aws-tf-modules/module.vpc-endpoints/data.tf
+++ aws-tf-modules/module.vpc-endpoints/data.tf
@@ -5,7 +5,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
+    bucket  = "${var.tfstate_s3_bucket_prefix}-tfstate-${var.default_region}"
     key     = "state/${var.environment}/vpc/terraform.tfstate"
     region  = var.default_region
   }
