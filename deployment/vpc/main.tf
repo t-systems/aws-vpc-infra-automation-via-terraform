a60e80d9e833b211f97aaa3fa2342728cb8bfc9b
vivek22117 <vivekmishra22117@gmail.com>
2021-09-04 16:50:45 +0530

    implemnting VPC

---
+++ deployment/vpc/main.tf
@@ -0,0 +1,25 @@
+####################################################
+#           VPC module deployment                  #
+####################################################
+module "vpc" {
+  source = "../../aws-tf-modules/module.vpc"
+
+  environment    = var.environment
+  default_region = var.default_region
+
+  cidr_block            = var.cidr_block
+  private_azs_with_cidr = var.private_azs_with_cidr
+  public_azs_with_cidr  = var.public_azs_with_cidr
+  db_azs_with_cidr      = var.db_azs_with_cidr
+  instance_tenancy      = var.instance_tenancy
+  enable_dns            = var.enable_dns
+  support_dns           = var.support_dns
+  enable_nat_gateway    = var.enable_nat_gateway
+
+  team         = var.team
+  owner        = var.owner
+  isMonitoring = var.isMonitoring
+  project      = var.project
+
+  bastion_instance_type = var.bastion_instance_type
+}
