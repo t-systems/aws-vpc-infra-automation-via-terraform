adcffb4c55ed3b38c1ad4305936a49a7a4d0b6d7
vivek22117 <vivekmishra22117@gmail.com>
2021-09-06 12:22:43 +0530

    adding TF provider

---
+++ deployment/s3-buckets/providers.tf
@@ -0,0 +1,55 @@
+####################################################
+# AWS provider configuration                       #
+####################################################
+provider "aws" {
+  region = var.default_region
+  //  profile = "default"
+
+  version = ">=2.28.0"
+}
+
+
+###########################################################
+# Terraform configuration block is used to define backend #
+# Interpolation syntax is not allowed in Backend          #
+###########################################################
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.3"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+  }
+  required_version = ">= 0.13"
+
+
+  backend "s3" {
+    //    profile = "default"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+
+}
+
+# used for accessing Account ID and ARN
+data "aws_caller_identity" "current" {}
