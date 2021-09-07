7781949cd41c7b6ee15abf4d13326520d516ef8f
vivek22117 <vivekmishra22117@gmail.com>
2021-09-07 13:29:59 +0530

    updating output for s3 modue

--- aws-tf-modules/module.aws-s3-buckets/output.tf
+++ aws-tf-modules/module.aws-s3-buckets/output.tf
@@ -1,23 +1,23 @@
 output "artifactory_s3_arn" {
-  value = aws_s3_bucket.s3_artifactory_bucket.arn
+  value = aws_s3_bucket.s3_bucket["artifactory"].arn
 }
 
 output "logging_s3_arn" {
-  value = aws_s3_bucket.s3_logging_bucket.arn
+  value = aws_s3_bucket.s3_bucket["logging"].arn
 }
 
 output "datalake_s3_arn" {
-  value = aws_s3_bucket.s3_dataLake_bucket.arn
+  value = aws_s3_bucket.s3_bucket["datalake"].arn
 }
 
 output "artifactory_s3_name" {
-  value = aws_s3_bucket.s3_artifactory_bucket.id
+  value = aws_s3_bucket.s3_bucket["artifactory"].id
 }
 
 output "logging_s3_name" {
-  value = aws_s3_bucket.s3_logging_bucket.id
+  value = aws_s3_bucket.s3_bucket["logging"].id
 }
 
 output "datalake_s3_name" {
-  value = aws_s3_bucket.s3_dataLake_bucket.id
+  value = aws_s3_bucket.s3_bucket["datalake"].id
 }
