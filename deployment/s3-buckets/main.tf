26abf04050f3a4fe382cb847326b6ca3d3a095c0
vivek22117 <vivekmishra22117@gmail.com>
2021-09-07 13:30:59 +0530

    updating main file for TF s3

--- deployment/s3-buckets/main.tf
+++ deployment/s3-buckets/main.tf
@@ -6,14 +6,12 @@ module "s3_resources" {
 
   default_region = var.default_region
 
-  artifactory_bucket_prefix = var.artifactory_bucket_prefix
-  logging_bucket_prefix     = var.logging_bucket_prefix
-  dataLake_bucket_prefix    = var.dataLake_bucket_prefix
-
   team         = var.team
   owner        = var.owner
   environment  = var.environment
   isMonitoring = var.isMonitoring
   component    = var.component
   project      = var.project
+
+  bucket_config = var.bucket_config
 }
