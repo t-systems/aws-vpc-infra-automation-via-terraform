07dc0df4189f6552543570e080eed4846a26a1e2
vivek22117 <vivekmishra22117@gmail.com>
2021-09-08 21:25:50 +0530

    removing unwanted installation

--- packer/bastion/setup.sh
+++ packer/bastion/setup.sh
@@ -4,16 +4,6 @@ sudo yum update -y
 sudo yum install unzip -y
 sleep 5
 
-
-echo "Install Telegraf"
-wget https://dl.influxdata.com/telegraf/releases/telegraf-1.6.0-1.x86_64.rpm -O /tmp/telegraf.rpm
-yum localinstall -y /tmp/telegraf.rpm
-rm /tmp/telegraf.rpm
-chkconfig telegraf on
-mv /tmp/telegraf.conf /etc/telegraf/telegraf.conf
-service telegraf start
-
-
 echo "Install SSM-Agent"
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl enable amazon-ssm-agent
@@ -25,34 +15,5 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo  ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
 
-
-echo "Install NodeExporter"
-cd /tmp
-wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
-tar -xf node_exporter-0.18.1.linux-amd64.tar.gz
-sudo mv node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin/
-sudo useradd -rs /bin/false node_exporter
-chown node_exporter:node_exporter /usr/local/bin/node_exporter
-rm -rf /tmp/node_exporter-0.18.1.linux-amd64*
-
-sudo cat << EOF > /etc/systemd/system/node_exporter.service
-[Unit]
-Description=Node Exporter
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-User=node_exporter
-Group=node_exporter
-Type=simple
-ExecStart=/usr/local/bin/node_exporter
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl start node_exporter
-sudo systemctl enable node_exporter
 
 echo "SUCCESS! Installation succeeded!"
