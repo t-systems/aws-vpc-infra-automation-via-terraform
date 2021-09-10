1be4c83787440a30b665aec34d1f0c92183d3128
vivek22117 <vivekmishra22117@gmail.com>
2021-09-10 09:05:21 +0530

    updating backend providers file

--- aws-terraform-backend/providers.tf
+++ aws-terraform-backend/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = var.default_region
-  profile = "default"
+#  profile = "default"
 
   version = ">=2.22.0"
 }
@@ -8,6 +8,11 @@ provider "aws" {
 terraform {
   required_version = ">= 0.13"
 
+  backend "s3" {
+#    profile        = "default"
+    region         = "us-east-1"
+    encrypt        = "true"
+  }
 }
 
 data "aws_caller_identity" "current" {}
